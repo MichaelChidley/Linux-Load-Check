0860ef2870cc0657606fcee006fc1963071493b3
MichaelChidley <michael24687@hotmail.com>
2013-10-20 14:07:07 +0100

    Minor Updates
    Updated so the email included information on the tasks using the highest
    amount of CPU usage

--- loadCheck.sh
+++ loadCheck.sh
@@ -10,15 +10,20 @@
 #Store the command to get the load into a variable
 CURRENTLOAD=`uptime | awk -F, '{print $4}' | awk '{print $3}'`
 
+#Top load processes
+TOPLOAD=`ps -eo pcpu,pid,user,args | sort -k 1 -r | head -10`
+
 #Create an email variable to hold the recipient email address
 EMAIL=""
 
 #Set the max load limit to send email if greater than
-MAXLOAD=0.75
+MAXLOAD=0.50
 
 #Store the hostname command into a variable
 HOSTNAME=`hostname`
 
 if [[ $CURRENTLOAD > $MAXLOAD ]]; then
-        echo "High load detected on $HOSTNAME ($CURRENTLOAD)" | mutt -s "Abnormal Load Detected" -- $EMAIL
+        echo "High load detected on $HOSTNAME ($CURRENTLOAD) 
+
+$TOPLOAD" | mutt -s "Abnormal Load Detected" -- $EMAIL
 fi
